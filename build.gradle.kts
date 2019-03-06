b9c6432ede164cb4b7b2a22cb66ea8897f06a136
Chris Muthig <camuthig@gmail.com>
2019-03-06 19:35:15 +1030

    Add configurations to publish 0.1.0 to Bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,19 @@
+import com.jfrog.bintray.gradle.BintrayExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.20"
-    `java-gradle-plugin`
+    `java-library`
     `maven-publish`
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
 group = "org.camuthig.credentials"
-version = "0.1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
@@ -34,6 +37,16 @@ kotlin.sourceSets["test"].kotlin.srcDirs("test")
 sourceSets["main"].resources.srcDirs("resources")
 sourceSets["test"].resources.srcDirs("test/resources")
 
+tasks.register<Jar>("sourcesJar") {
+    from(sourceSets.main.get().allJava)
+    archiveClassifier.set("sources")
+}
+
+tasks.register<Jar>("javadocJar") {
+    from(tasks.javadoc)
+    archiveClassifier.set("javadoc")
+}
+
 publishing {
     repositories {
         maven {
@@ -43,8 +56,49 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
 
             artifactId = "core"
+
+            pom {
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("camuthig")
+                        name.set("Chris Muthig")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/camuthig/kotlin-credentials")
+                }
+            }
         }
     }
 }
+
+bintray {
+    user = System.getProperty("bintray.user")
+    key = System.getProperty("bintray.key")
+    publish = true
+    setPublications("maven")
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        repo = "maven"
+        name = "credentials-core"
+        websiteUrl = "https://camuthig.dev"
+        githubRepo = "camuthig/kotlin-credentials"
+        vcsUrl = "https://github.com/camuthig/kotlin-credentials"
+        description = "A library for storing credentials within a project in an encrypted file"
+        setLabels("kotlin")
+        setLicenses("Apache-2.0")
+        desc = description
+    })
+}
+
